59f2e3dbbdc55bcac57a7f29447db1595041d5ef
AllanMonteiro <engflorestal.allan@gmail.com>
2024-04-20 23:49:34 -0300

    Versao2 Fake

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-print('Teste')
+#importar bliblioteca Faker
+from faker import Faker
+
+#crie uma instancia do faker
+
+fake = Faker()
+
+#Gere um nome ficiticio
+
+nome = fake.name()
+
+#gere um endereço ficiticio
+
+endereco = fake.address()
+
+
+#Gere um numero de telefone ficticio
+
+telefone = fake.phone_number()
+
+#gere uma data de nascimento ficticia( indade entre 18 e 65 anos)
+
+data_de_nascimento = fake.date_of_birth(minimum_age = 18, maximum_age = 65)
+
+
+#imprima os dados ficticios
+
+print('Nome: ', nome)
+print('Endereço: ', endereco)
+print('Telefone: ', telefone)
+print('Data de nascimento: ', data_de_nascimento)
